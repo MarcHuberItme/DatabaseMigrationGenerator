a74ccb9f9cac09bb0e9d0063bc231636469edaeb
Huber Ivan <ivan.huber@hbl.ch>
2026-01-19 14:36:20 +0100

    Introduce output of appsettings

--- DatabaseMigrationGenerator/Test.cs
+++ DatabaseMigrationGenerator/Test.cs
@@ -1,9 +1,31 @@
-﻿namespace DatabaseMigrationGenerator;
+﻿using DatabaseMigrationGenerator.Settings;
+using Microsoft.Extensions.Options;
 
-public class Test : ITest
+namespace DatabaseMigrationGenerator;
+
+public class Test(
+    IOptions<LiquibaseSettings> liquibase,
+    IOptions<SqlServerSettings> sql,
+    IOptions<MiscSettings> misc)
+    : ITest
 {
+
     public void Write()
     {
-        Console.WriteLine("Test text written!");
+        Console.WriteLine($"Liquibase Settings: Run:{liquibase.Value.Run}");
+        Console.WriteLine($"Liquibase Settings: ExecutablePath:{liquibase.Value.ExecutablePath}");
+        Console.WriteLine($"Liquibase Settings: WorkingDirectory:{liquibase.Value.WorkingDirectory}");
+        Console.WriteLine($"Liquibase Settings: ContextFilter:{liquibase.Value.ContextFilter}");
+        Console.WriteLine($"Liquibase Settings: LabelFilter:{liquibase.Value.LabelFilter}");
+        
+        Console.WriteLine($"SqlServer Settings: HostName:{sql.Value.HostName}");
+        Console.WriteLine($"SqlServer Settings: InstanceName:{sql.Value.InstanceName}");
+        Console.WriteLine($"SqlServer Settings: DatabaseName:{sql.Value.DatabaseName}");
+        Console.WriteLine($"SqlServer Settings: Encrypt:{sql.Value.Encrypt}");
+        Console.WriteLine($"SqlServer Settings: TrustedServerCertificate:{sql.Value.TrustedServerCertificate}");
+        Console.WriteLine($"SqlServer Settings: IntegratedSecurity:{sql.Value.IntegratedSecurity}");
+        
+        Console.WriteLine($"Misc Settings: DoNotCreateTriggersForTables:{misc.Value.DoNotCreateTriggersForTables}");
+        Console.WriteLine($"Misc Settings: DoNotCreateGetDetailAndGetListForStoredProcedures:{misc.Value.DoNotCreateGetDetailAndGetListForStoredProcedures}");
     }
 }
